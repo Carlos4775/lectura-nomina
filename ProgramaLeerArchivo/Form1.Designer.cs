d90d265b2d915e8b4070c3c74b56dcb90f1fb2d6
Carlos Torres Graciano <carlosmiguel_torres@bhdleon.com.do>
2022-09-30 22:32:53 -0400

    first commit

---
+++ ProgramaLeerArchivo/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ProgramaLeerArchivo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Lucida Bright", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(23, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cargar archivo";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.Color.RoyalBlue;
+            this.dataGridView1.Location = new System.Drawing.Point(42, 155);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1268, 391);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(42, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(111, 94);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(193, 31);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(911, 94);
+            this.panel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1352, 616);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Lectura de Nominas";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ ProgramaLeerArchivo/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramaLeerArchivo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string line;
+            DataTable dt = new DataTable();
+            dt.Clear();
+            dt.Columns.Add("Clave_Nomina");
+            dt.Columns.Add("Tipo_Doc");
+            dt.Columns.Add("No_Doc");
+            dt.Columns.Add("Nombre");
+            dt.Columns.Add("Primer_Apellido");
+            dt.Columns.Add("Segundo_Apellido");
+            dt.Columns.Add("Sexo");
+            dt.Columns.Add("Fecha_Nacimiento");
+            dt.Columns.Add("Salario_Cotizable");
+            dt.Columns.Add("Aporte_Voluntario");
+            dt.Columns.Add("Salario_ISR");
+            dt.Columns.Add("Otras_Remuneraciones");
+            dt.Columns.Add("RNC_Cedula");
+            dt.Columns.Add("Ingresos_Exentos");
+            dt.Columns.Add("Saldo_Favor");
+            dt.Columns.Add("Salario_INFOTEP");
+            dt.Columns.Add("Tipo_Ingreso");
+            // lee linea a linea 
+            System.IO.StreamReader file = new System.IO.StreamReader(@"c:\files\Nomina.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] registro = line.Split(',');
+                DataRow fila = dt.NewRow();
+                fila["Clave_Nomina"] = registro[0];
+                fila["Tipo_Doc"] = registro[1];
+                fila["No_Doc"] = registro[2];
+                fila["Nombre"] = registro[3];
+                fila["Primer_Apellido"] = registro[4];
+                fila["Segundo_Apellido"] = registro[5];
+                fila["Sexo"] = registro [6];
+                fila["Fecha_Nacimiento"] = registro[7];
+                fila["Salario_Cotizable"] = registro[8];
+                fila["Aporte_Voluntario"] = registro[9];
+                fila["Salario_ISR"] = registro[10];
+                fila["Otras_Remuneraciones"] = registro[11];
+                fila["RNC_Cedula"] = registro[12];
+                fila["Ingresos_Exentos"] = registro[13];
+                fila["Saldo_Favor"] = registro[14];
+                fila["Salario_INFOTEP"] = registro[15];
+                fila["Tipo_Ingreso"] = registro[16];
+                dt.Rows.Add(fila);
+            }
+
+            file.Close();
+            dataGridView1.DataSource = dt;
+            dataGridView1.Refresh();
+        }
+    }
+}
